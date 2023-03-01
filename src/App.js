32f4404c54b080f9a1a864965c78fac47e9b9fa6
Nicolas Torres <desmondgamesyt@gmail.com>
2023-03-01 14:54:25 -0300

    added categories to app component.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import {  Header } from './components/header/Header';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import { Header } from "./components/header/Header";
+import { Categories } from "./components/categories/Categories";
 function App() {
   return (
     <>
-    <Header/>
-    <div className="App">
+      <Header />
+      <div className="App">
         {/* <Counter /> */}
-    </div>
+        <div></div>
+        <div>
+          <Categories />
+        </div>
+      </div>
     </>
   );
 }
