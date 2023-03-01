093f49df4f777c376f22eec2743a497cd91b30d2
Nicolas Torres <desmondgamesyt@gmail.com>
2023-03-01 13:39:18 -0300

    imported header component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-
+import {  Header } from './components/header/Header';
 function App() {
   return (
+    <>
+    <Header/>
     <div className="App">
         {/* <Counter /> */}
     </div>
+    </>
   );
 }
 
