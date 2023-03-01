5d564a1debc4ffe934f48e7a45b0128bbc398f38
Nicolas Torres <desmondgamesyt@gmail.com>
2023-03-01 14:53:34 -0300

    added a categories component

---
+++ src/components/categories/Categories.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Categories.css";
+
+export function Categories() {
+  return (
+    <nav>
+        <h2>The choices we make, determine our destiny</h2>
+        <span>choose wisely</span>
+      <ul>
+        <li></li>
+      </ul>
+    </nav>
+  );
+}
