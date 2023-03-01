9ec7214498d1bb610a44e9ad786d27fcba30d6f7
Nicolas Torres <desmondgamesyt@gmail.com>
2023-03-01 18:45:07 -0300

    added a function to fetch posts from reddit

--- src/features/posts/fetchPosts.js
+++ src/features/posts/fetchPosts.js
@@ -1,9 +1,21 @@
 
+export async function toShowPosts(category = 'ProgrammerHumor') {
+    let response = await fetch(`https://www.reddit.com/r/${category}/new.json?limit=10`);
+    let data = await response.json();
+    let initialState = data.data.children.map((post) => {
+      return {
+        imgUrl: post.data.url,
+        author: post.data.author,
+        title: post.data.title,
+        link: `https://www.reddit.com/${post.data.permalink}`,
+        upVotes: post.data.ups,
+        downVotes: post.data.downs,
+        subReddit: post.data.subreddit,
+        subRedditPrefix: post.data.subreddit_name_prefixed,
+      };
+    });
+    return initialState;
+  }
 
 
-export async function toShowPosts(category = 'ProgrammerHumor'){
-    let response = await fetch(`https://www.reddit.com/r/${category}/new.json?limit=1`)
-    response = await response.json()
-    return response.data
-}
 
