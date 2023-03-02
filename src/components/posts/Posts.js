970e99d64a0a53b7022d67440a6e2f1d72e7a3cb
Nicolas Torres <desmondgamesyt@gmail.com>
2023-03-01 21:01:20 -0300

    added functions for up votes and downvotes, and also style

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,26 +1,102 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { selectPosts } from '../../features/posts/PostsSlice'
-import { asyncPosts } from '../../features/posts/PostsSlice'
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { selectPosts } from "../../features/posts/PostsSlice";
+import { asyncPosts } from "../../features/posts/PostsSlice";
+import { ArrowUp, ArrowDown } from "react-feather";
+import "./posts.css";
 export function Posts() {
-    const dispatch = useDispatch();
-    const posts = useSelector(selectPosts);
-    useEffect(() => {
-        dispatch(asyncPosts());
-    },[])
-    return (
-        <div>
-            {posts.map((post) => {
-                return (
-                    <div key={post.link}>
-                        <span>{post.title}</span>
-                        <span>{post.upVotes}</span>
-                        <span>{post.downVotes}</span>
-                        <a href={post.link} target="_blank"><img src={post.imgUrl} alt={post.title} /></a>
-                        <span>{post.subRedditPrefix}</span>
-                        <span>{post.author}</span>
-                        </div>
-                )})}
-        </div>
-    )
+  const dispatch = useDispatch();
+  const posts = useSelector(selectPosts);
+  useEffect(() => {
+    dispatch(asyncPosts());
+  }, []);
+  const [greenPressed, setGreenPressed] = useState(false);
+  const [redPressed, setRedPressed] = useState(false);
+  function handleUp(e) {
+    if (greenPressed) {
+      e.currentTarget.parentElement.parentElement.querySelector(
+        "#upvotesnum"
+      ).innerHTML =
+        posts[e.currentTarget.attributes.id.value.split(" ")[1]].upVotes;
+    }
+    e.currentTarget.firstChild.style.color = "#05c224";
+    e.currentTarget.nextSibling.firstChild.style.color = "#000";
+    let currentNum =
+      e.currentTarget.parentElement.parentElement.querySelector(
+        "#upvotesnum"
+      ).textContent;
+    e.currentTarget.parentElement.parentElement.querySelector(
+      "#upvotesnum"
+    ).innerHTML = parseInt(currentNum) + 1;
+    e.currentTarget.parentElement.parentElement.querySelector(
+      "#downvotesnum"
+    ).innerHTML =
+      posts[e.currentTarget.attributes.id.value.split(" ")[1]].downVotes;
+
+    setGreenPressed(true);
+  }
+  function handleDown(e) {
+    if (redPressed) {
+      e.currentTarget.parentElement.parentElement.querySelector(
+        "#downvotesnum"
+      ).innerHTML =
+        posts[e.currentTarget.attributes.id.value.split(" ")[1]].downVotes;
+    }
+    e.currentTarget.firstChild.style.color = "#d52619";
+    e.currentTarget.previousSibling.firstChild.style.color = "#000";
+    let currentNum =
+      e.currentTarget.parentElement.parentElement.querySelector(
+        "#downvotesnum"
+      ).textContent;
+    e.currentTarget.parentElement.parentElement.querySelector(
+      "#downvotesnum"
+    ).innerHTML = parseInt(currentNum) - 1;
+    e.currentTarget.parentElement.parentElement.querySelector(
+      "#upvotesnum"
+    ).innerHTML =
+      posts[e.currentTarget.attributes.id.value.split(" ")[1]].upVotes;
+
+    setRedPressed(true);
+  }
+  return (
+    <>
+      {posts.map((post, i) => {
+        return (
+          <div className={`post`} key={post.link}>
+            <div className="downUp">
+              <span id="upvotesnum">{post.upVotes}</span>
+              <div onClick={handleUp} id={`uparrow ${i}`}>
+                <ArrowUp></ArrowUp>
+              </div>
+              <div onClick={handleDown} id={`downarrow ${i}`}>
+                <ArrowDown></ArrowDown>
+              </div>
+              <span id="downvotesnum">{post.downVotes}</span>
+            </div>
+            <div className="card-center">
+              <span
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  padding: "8px 0px",
+                  fontSize: 21,
+                  fontWeight: 600,
+                  opacity: 0.7,
+                }}
+              >
+                {post.title}
+              </span>
+              <a href={post.link} target="_blank">
+                <img src={post.imgUrl} alt={post.title} />
+              </a>
+              <div className="author-sub">
+                <span>{post.subRedditPrefix}</span>
+                <span>{post.author}</span>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
 }
