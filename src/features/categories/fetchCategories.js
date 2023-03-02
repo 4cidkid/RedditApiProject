ac6e4c54b8b62bd7712df451a58a9c22de717248
Nicolas Torres <desmondgamesyt@gmail.com>
2023-03-01 22:28:54 -0300

    modified the funcion to fetch categories to one with redux

--- src/features/categories/fetchCategories.js
+++ src/features/categories/fetchCategories.js
@@ -1,19 +1,48 @@
-const categoriesToFetch = ['programming','learnprogramming','ProgrammerHumor','AskProgramming','programminghorror','programmingcirclejerk','programmingmemes','programmingjokes','programmingdadjokes'];
+const categoriesToFetch = ['programming','learnprogramming','ProgrammerHumor','AskProgramming','programminghorror','programmingcirclejerk','programmingmemes','programmingjokes','ProgrammerDadJokes'];
 
-
-const categoriesObj = [];
-
-
-
-
-const fetchCategories = async () => {
-    categoriesToFetch.forEach(async (category) => {
-        const response = fetch(`https://www.reddit.com/r/${category}/about.json`)
-        const data = await response.json();
-        categoriesObj.push({
-            name: data.data.display_name,
-            title: data.data.title,
-            icon: data.data.icon_img,
-        });
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+export const asyncCategories = createAsyncThunk(
+    'categories/loadCategories',
+    async (arg) => {
+        let initialState = []
+        categoriesToFetch.forEach(async (category) => {
+            const response = fetch(`https://www.reddit.com/r/${category}/about.json`)
+            const data = await response.json();
+            initialState.push({
+                name: data.data.display_name,
+                title: data.data.title,
+                icon: data.data.icon_img,
+            });
+        })
+        return initialState
     })
-}
+export const categoriesSlice = createSlice({
+    name: 'categories',
+    initialState: {
+        categories: [],
+        isLoading: false,
+        hasErrors: false,
+    },
+    reducers:{
+        // loadPost: (state, action) => {
+    },
+    extraReducers:{
+        [asyncCategories.pending]: (state) => {
+            state.isLoading = true;
+            state.hasErrors = false;
+        },
+        [asyncCategories.fulfilled]: (state, action) => {
+            state.isLoading = false;
+            state.hasErrors = false;
+            state.posts = action.payload;
+        },
+        [asyncCategories.rejected]: (state) => {
+            state.isLoading = false;
+            state.hasErrors = true;
+        }
+    }
+})
+export const { } = categoriesSlice.actions;
+export const selectCategories = (state) => state.categories.categories
+export const selectLoading = (state) => state.isLoading
+export default categoriesSlice.reducer;
