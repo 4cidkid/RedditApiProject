09d968f52317fbec8166c364be365be3bc07b77c
Nicolas Torres <desmondgamesyt@gmail.com>
2023-03-01 22:09:37 -0300

    added a conditional statement to show the content, only if is loaded

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectPosts } from "../../features/posts/PostsSlice";
 import { asyncPosts } from "../../features/posts/PostsSlice";
+import { selectLoading } from "../../features/posts/PostsSlice";
 import { ArrowUp, ArrowDown } from "react-feather";
 import "./posts.css";
 export function Posts() {
   const dispatch = useDispatch();
   const posts = useSelector(selectPosts);
+  const loading = useSelector(selectLoading);
+  console.log(posts)
   useEffect(() => {
     dispatch(asyncPosts());
   }, []);
@@ -60,43 +63,48 @@ export function Posts() {
   }
   return (
     <>
-      {posts.map((post, i) => {
-        return (
-          <div className={`post`} key={post.link}>
-            <div className="downUp">
-              <span id="upvotesnum">{post.upVotes}</span>
-              <div onClick={handleUp} id={`uparrow ${i}`}>
-                <ArrowUp></ArrowUp>
-              </div>
-              <div onClick={handleDown} id={`downarrow ${i}`}>
-                <ArrowDown></ArrowDown>
-              </div>
-              <span id="downvotesnum">{post.downVotes}</span>
+      {loading ? (
+        <div className="loading">
+            <span>Loading...</span>
+            <img src="https://media.tenor.com/RVvnVPK-6dcAAAAM/reload-cat.gif" alt="loading-cat"></img>
             </div>
-            <div className="card-center">
-              <span
-                style={{
-                  width: "100%",
-                  display: "flex",
-                  padding: "8px 0px",
-                  fontSize: 21,
-                  fontWeight: 600,
-                  opacity: 0.7,
-                }}
-              >
-                {post.title}
-              </span>
-              <a href={post.link} target="_blank">
-                <img src={post.imgUrl} alt={post.title} />
-              </a>
-              <div className="author-sub">
-                <span>{post.subRedditPrefix}</span>
-                <span>{post.author}</span>
-              </div>
-            </div>
-          </div>
-        );
-      })}
+            ) : (posts.map((post, i) => {
+                return (
+                  <div className={`post`} key={post.link}>
+                    <div className="downUp">
+                      <span id="upvotesnum">{post.upVotes}</span>
+                      <div onClick={handleUp} id={`uparrow ${i}`}>
+                        <ArrowUp></ArrowUp>
+                      </div>
+                      <div onClick={handleDown} id={`downarrow ${i}`}>
+                        <ArrowDown></ArrowDown>
+                      </div>
+                      <span id="downvotesnum">{post.downVotes}</span>
+                    </div>
+                    <div className="card-center">
+                      <span
+                        style={{
+                          width: "100%",
+                          display: "flex",
+                          padding: "8px 0px",
+                          fontSize: 21,
+                          fontWeight: 600,
+                          opacity: 0.7,
+                        }}
+                      >
+                        {post.title}
+                      </span>
+                      <a href={post.link} target="_blank">
+                        <img src={post.imgUrl} alt={post.title} />
+                      </a>
+                      <div className="author-sub">
+                        <span>{post.subRedditPrefix}</span>
+                        <span>{post.author}</span>
+                      </div>
+                    </div>
+                  </div>
+                );
+              }))}
     </>
   );
 }
